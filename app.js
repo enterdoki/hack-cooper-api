c26789a6d78164374ebec8ae734a21ddada8b82e
Toms <chen.tommy925@gmail.com>
2019-09-06 19:56:26 -0400

    login, token done

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
-require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
-
+const person = require('./routes/user')
+const db = require('./database/db')
 app.use(cors());
+app.use('/user', person);
+
 app.get('/', (req, res, next) =>{
-    res.send('Hello World!')
+    res.status(200).send('This is the default API.')
 }) 
+
+// db.sync({force:true});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
