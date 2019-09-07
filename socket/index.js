ec83f3eac100ad5838826a605f57ba9081d5a72e
Steven Li <ilumii@users.noreply.github.com>
2019-09-06 21:48:23 -0400

    test

--- socket/index.js
+++ socket/index.js
@@ -1,8 +1,13 @@
+const roomHandler = require('./roomHandler');
 module.exports = (io) => {
-
     io.on('connection', (socket) => {
         console.log('a user connected');
         
+        socket.on('join', async (user, room) =>{
+         //   let room = roomHandler.addRoom(room);
+            socket.broadcast.emit('new', {description: "new user connected"});
+        });
+
         socket.on('disconnect', () => {
             console.log('user disconnected');
         });
